3530c91c99d4d7f33a274280011c084c0e553f9f
Anton Illarionov <anton.illarionov1991@gmail.com>
2022-08-04 20:34:50 +0300

    F1

--- src/test/java/ManagerProductTest.java
+++ src/test/java/ManagerProductTest.java
@@ -6,7 +6,7 @@ public class ManagerProductTest {
     Book book2= new Book(2, "Литература", 3500, "Сидоров С.С.");
     Book book3= new Book(3, "Аллегории", 3000, "Петров П.П.");
     Book book4= new Book(4, "Алгебра", 1600, "Прохоров А.В.");
-    Smartphone smartphone1 = new Smartphone(5, "Рандаунд", 80_000, "Huawei");
+    Smartphone smartphone1 = new Smartphone(5, "Рандаунд ", 80_000, "Huawei");
     Smartphone smartphone2 = new Smartphone(6, "ГАлакси", 95_000, "Samsung");
 
 
@@ -45,6 +45,30 @@ public class ManagerProductTest {
 
         Product[] actual =  manager.searchBy("Ал");
         Product[]expected = {book3,book4,smartphone2};
+
+        Assertions.assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    public void find3ProductTest(){
+        manager.add(book3);
+        manager.add(book4);
+        manager.add(smartphone2);
+
+        Product[] actual =  manager.searchBy("Матр");
+        Product[]expected = {};
+
+        Assertions.assertArrayEquals(actual, expected);
+    }
+
+    @Test
+    public void find4ProductTest(){
+        manager.add(book1);
+        manager.add(book3);
+        manager.add(smartphone1);
+
+        Product[] actual =  manager.searchBy("язык");
+        Product[]expected = {book1};
 
         Assertions.assertArrayEquals(actual, expected);
     }
